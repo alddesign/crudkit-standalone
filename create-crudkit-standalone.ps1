bed3a45251a021d817a80e7fb5a58329adc24170
alddesign <alexander.doepper@aon.at>
2021-07-09 21:28:16 +0200

    Update

--- create-crudkit-standalone.ps1
+++ create-crudkit-standalone.ps1
@@ -1,3 +1,4 @@
+# !!! Do not run. Administrative tool for building this archive. !!!
 #Run PHPdoc cmd
 cd $PSScriptRoot
 Write-Host "Create Phpdoc..." -ForegroundColor Yellow;
@@ -8,10 +9,10 @@ cd $PSScriptRoot
 #Remove unwanted files
 Write-Host "Deleting unwanted files..." -ForegroundColor Yellow;
 Get-ChildItem -Path ..\crudkit\storage\framework\sessions -Exclude (".gitignore") | Remove-Item
-Get-ChildItem -Path ..\crudkit\storage\framework\cache -Exclude (".gitignore") | Remove-Item
+Get-ChildItem -Path ..\crudkit\storage\framework\cache -Exclude (".gitignore") | Remove-Item -Recurse
 Get-ChildItem -Path ..\crudkit\storage\framework\views -Exclude (".gitignore") | Remove-Item
 Get-ChildItem -Path ..\crudkit\storage\logs -Exclude (".gitignore") | Remove-Item
-Get-ChildItem -Path ..\crudkit\storage\crudkit | Remove-Item
+Get-ChildItem -Path ..\crudkit\storage\crudkit -Recurse | Remove-Item
 Remove-Item -ErrorAction Ignore -Path .\crudkit.zip
 
 Write-Host "Creating Zip..." -ForegroundColor Yellow;
@@ -19,4 +20,3 @@ Set-Alias -Name 7z -Value 'C:\Program Files\7-Zip\7z.exe'
 7z a -tzip ".\crudkit.zip" "..\crudkit" -xr@"exclude.txt"
 
 Write-Host "Crudkit-Standalone created" -ForegroundColor Green;
-Read-Host
