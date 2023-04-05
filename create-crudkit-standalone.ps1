4c51df793922adc797cb58757050cf842bd1dfeb
alddesign <alexander@alddesign.at>
2023-04-06 00:34:35 +0200

    update

--- create-crudkit-standalone.ps1
+++ create-crudkit-standalone.ps1
@@ -1,12 +1,17 @@
-# !!! Do not run. Administrative tool for building this archive. !!!
-#Run PHPdoc cmd
-cd $PSScriptRoot
-Write-Host "Create Phpdoc..." -ForegroundColor Yellow;
-cd "..\crudkit\vendor\alddesign\crudkit\doc\";
-.\create_phpdoc.ps1
+<# 
+!!! Do not run. 
+Administrative tool for building this archive.
+I want that shit in the repo, cause i need it...
+#>
 cd $PSScriptRoot
 
+#Run PHPdoc
+cd "..\crudkit\vendor\alddesign\crudkit\doc\";
+Write-Host "Create Phpdoc..." -ForegroundColor Yellow;
+.\create_phpdoc.ps1
+
 #Remove unwanted files
+cd $PSScriptRoot
 Write-Host "Deleting unwanted files..." -ForegroundColor Yellow;
 Get-ChildItem -Path ..\crudkit\storage\framework\sessions -Exclude (".gitignore") | Remove-Item
 Get-ChildItem -Path ..\crudkit\storage\framework\cache -Exclude (".gitignore") | Remove-Item -Recurse
